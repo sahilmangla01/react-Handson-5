801121a05f64a675e930e1c17329eb3b76beeb28
sahilmangla01 <manglasahil9050@gmail.com>
2023-08-01 12:31:19 +0530

    first

---
+++ src/Component/RouteComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Routes ,Route} from "react-router-dom";
+
+import PureComponent from "./PureComponent";
+import Home from "./Home";
+import HocDisplay from "./HocDisplay";
+
+
+const RouteComponent =()=>{
+
+    return(
+        <>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/hoc" element={<HocDisplay/>}></Route>
+                <Route path="/pure_component" element={<PureComponent/>}></Route>
+
+            </Routes>
+        
+        </>
+    )
+
+}
+export default RouteComponent
